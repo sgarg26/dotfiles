83c0d27bb07dae19629f48a006f258205df07758
sgarg26 <siddharth261202@gmail.com>
2023-12-21 02:16:19 +0530

    reordered mappings

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -1,7 +1,11 @@
 local keymap = vim.keymap.set
--- keymaps
-keymap("n", "<Esc>", "<cmd>noh<cr>", { silent = true } )
+
+-- General keymaps
+keymap("n", "<Esc>", "<cmd>noh<cr>", { silent = true } ) -- map esc to no highlighting
+keymap("i", "<S-Tab>", "<C-d>", { silent = true } ) -- map shift-tab to reverse indent like vscode
+
+-- Bufferline keymaps
 keymap("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", { silent = true } )
 keymap("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { silent = true } )
 keymap("n", "<leader>x", "<cmd>bdelete!<cr>", { silent = true } )
-keymap("i", "<S-Tab>", "<C-d>", { silent = true } )
+
