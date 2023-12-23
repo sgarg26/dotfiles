0206b25cd5e38052c29d6e4443392a2d6d5941f4
sgarg26 <siddharth261202@gmail.com>
2023-12-24 01:28:09 +0530

    added diagnostic signs

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -29,3 +29,33 @@ opt.signcolumn = "yes"
 
 -- Enable absolute line numbers
 vim.wo.nu = true
+-- Configuration for diagnostics
+local signs = {
+    { name = 'DiagnosticSignError', text = ' ' },
+    { name = 'DiagnosticSignWarn', text = ' ' },
+    { name = 'DiagnosticSignHint', text = '' },
+    { name = 'DiagnosticSignInfo', text = ' ' },
+}
+
+for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
+end
+
+local config = {
+    signs = {
+        active = signs, -- show signs
+    },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+        focusable = true,
+        style = 'minimal',
+        border = 'single',
+        source = 'always',
+        header = 'Diagnostic',
+        prefix = '',
+    },
+}
+
+vim.diagnostic.config(config)
