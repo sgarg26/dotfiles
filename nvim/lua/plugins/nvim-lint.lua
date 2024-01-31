089dd3c1aceaee7c74bddf01fd86cb13e78accd9
sgarg26 <siddharth261202@gmail.com>
2024-01-31 00:54:31 -0500

    Removed linting for js

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -1,12 +1,11 @@
 return {
-    "mfussenegger/nvim-lint",
-    ft = { "python", "c", "lua", "java" },
-    config = function()
-        require('lint').linters_by_ft = {
-            python = { "flake8" },
-            c = { "cpplint" },
-            lua = { "luacheck" },
-            javascript = { "eslint_d" }
-        }
-    end
+	"mfussenegger/nvim-lint",
+	ft = { "python", "c", "lua", "java" },
+	config = function()
+		require("lint").linters_by_ft = {
+			python = { "flake8" },
+			c = { "cpplint" },
+			lua = { "luacheck" },
+		}
+	end,
 }
