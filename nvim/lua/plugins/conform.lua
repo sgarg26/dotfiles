c1755a9e44a08dde0dcfe7b12a997c5d159b4029
sgarg26 <siddharth261202@gmail.com>
2024-01-31 11:37:18 -0500

    Removed haskell support

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,19 +1,20 @@
 return {
-    "stevearc/conform.nvim",
-    ft = { "python", "javascript", "lua", "c" },
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            -- Conform will run multiple formatters sequentially
-            python = { "isort", "black" },
-            -- Use a sub-list to run only the first available formatter
-            javascript = { { "prettierd", "prettier" } },
-            c = { "clang-format" }
-        },
-        format_on_save = {
-            -- These options will be passed to conform.format()
-            timeout_ms = 500,
-            lsp_fallback = true,
-        },
-    }
+	"stevearc/conform.nvim",
+	enabled = false,
+	ft = { "python", "javascript", "lua", "c" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Conform will run multiple formatters sequentially
+			python = { "isort", "black" },
+			-- Use a sub-list to run only the first available formatter
+			javascript = { { "prettierd", "prettier" } },
+			c = { "clang-format" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
