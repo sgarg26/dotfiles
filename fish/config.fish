9ee164694cc9e759e9bb6566d69c38c3d1e0fb29
sgarg26 <siddharth261202@gmail.com>
2024-02-11 02:25:16 -0500

    added prettierrc config file location

--- fish/config.fish
+++ fish/config.fish
@@ -13,6 +13,7 @@ alias bsync="browser-sync start --server --files '*.css'"
 set -g fish_greeting
 set PATH $HOME/.cargo/bin $PATH
 set PATH ~/.npm-global/bin $PATH
+set PRETTIERD_DEFAULT_CONFIG ~/.prettierrc.json
 starship init fish | source
 
 set -x MOZ_ENABLE_WAYLAND 1
