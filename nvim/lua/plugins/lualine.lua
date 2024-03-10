8d859c55c6d83ad9b1b1643a0a45e8000a2c4f4c
sgarg26 <siddharth261202@gmail.com>
2024-03-10 15:18:11 -0400

    added nvim icon to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,16 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = {
-        options = {
-            theme = "tokyonight",
-            disabled_filetypes = { statusline = { "dashboard" } }
-        }
-    }
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	opts = function()
+		return {
+			options = {
+				theme = "tokyonight",
+				disabled_filetypes = { statusline = { "dashboard" } },
+			},
+            -- stylua: ignore
+			sections = {
+				lualine_a = { function() return "" end, "mode" },
+			},
+		}
+	end,
 }
