66050ee913da1ae2297757cb7daaf4f10981d25c
sgarg26 <siddharth261202@gmail.com>
2024-05-05 14:35:48 -0400

    switched to astro

--- fish/config.fish
+++ fish/config.fish
@@ -2,19 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias sshzeus='kitten ssh sgarg21@zeus.vse.gmu.edu'
-alias vim=nvim
-alias ..='cd ..'
-alias cat=bat
-alias btui=bluetuith
-alias hx=helix
 set -g fish_greeting
+set -Ux EDITOR nvim
 set PATH $HOME/.cargo/bin $PATH
 set PATH $HOME/go/bin $PATH
 set PRETTIERD_DEFAULT_CONFIG ~/.prettierrc.json
 zoxide init fish | source
 starship init fish | source
+~/.local/bin/mise activate fish | source
 
 set -x MOZ_ENABLE_WAYLAND 1
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/sgarg26/.ghcup/bin # ghcup-env
